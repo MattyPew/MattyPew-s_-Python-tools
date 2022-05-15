d15cc013b15eb752b75a5c94732a2baa8f3c7f0b
Matěj Hlávka (MattyPew_) <80585569+NetysGamesCZ@users.noreply.github.com>
2022-05-15 10:54:32 +0200

    Update clear_console.py

--- clear_console.py
+++ clear_console.py
@@ -1,30 +1,15 @@
-
 # MODULES IMPORT
-
 #Platform import to detect os
 import platform
 # Importing clearing commands from system
 import sys, os
-# Sleep function
-from time import sleep
 
 # VARIABLES 
-
-#Tested print repeat volume
-x = 100
 #system = name of os
 system = platform.system()
-#Command variable for one-line clear (exec(cls))
+#Command variable for one-line clear exec(cls)
 cls = "Not set yet."
-
-# PRINTING TEST TEXT
-
-#Printing os name "x" times
-while x > 0:
-    print(system)
-    x = (x - 1)
-sleep(5)
-
+######################################
 #  SETTING CLS COMMAND
 
 #OS detection
@@ -37,8 +22,7 @@ elif system == "Linux":
 else:
     print("No operating system detected, cannot be cls command cannot be set...")
 
+######################################
 # EXECUTING CLS
-
-#Executing cls command
+# USE THIS CODE FOR CLEARING CONSOLE:
 exec(cls)
-sleep(5)
